5ef9c41329b350aeb1e47072d2e8d93b2f4489da
frinzekt <frinzekt@gmail.com>
2019-07-17 13:37:07 +0800

    VSCPT - Moving Code with ALT + (UP/DOWN Arrow Key)

--- Visual Studio Code Productivity Tips/Resource Files/MoveCode.js
+++ Visual Studio Code Productivity Tips/Resource Files/MoveCode.js
@@ -1,18 +1,14 @@
 function LiveDemo() {
-  var wholesaleCost = 45.89;
+	var wholesaleCost = 45.89;
 
-  var markup = .30;
-  var retail = wholesaleCost * markup;
-  var oddNumbers = new [1, 3, 5, 7];
+	var markup = 0.3;
+	var retail = wholesaleCost * markup;
+	var oddNumbers = new [1, 3, 5, 7]();
 
-  if (markup > 5. && wholesaleCost > 2000) {
-    if (((markup + 2) % 10) == 0) {
-      {
-
-      }
-
-    }
-
-  }
-
+	if (markup > 5 && wholesaleCost > 2000) {
+		if ((markup + 2) % 10 == 0) {
+			{
+			}
+		}
+	}
 }
