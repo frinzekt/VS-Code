f78708c5ba4051e645c4aad26ca2c8d91054293d
frinzekt <frinzekt@gmail.com>
2019-07-17 12:53:47 +0800

    Set up for course "Visual Studio Code Productivity Tips"

---
+++ Visual Studio Code Productivity Tips/Resource Files/Sort.js
@@ -0,0 +1,14 @@
+function LiveDemo()
+{
+
+  
+  
+  var bingo ;
+  var jello;
+  var arrayOfOddNumbers;
+  var markup = .3;
+  var longVariableName = "Long text";
+}
+
+function createCharArray(){}
+function addNumbers (){}
