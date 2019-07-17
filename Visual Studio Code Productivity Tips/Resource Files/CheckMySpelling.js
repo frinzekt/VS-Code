c3e3c5ad8817803eebb36fbb386b49369e7c4f43
frinzekt <frinzekt@gmail.com>
2019-07-17 13:34:57 +0800

    VSCPT - Spell Checker

--- Visual Studio Code Productivity Tips/Resource Files/CheckMySpelling.js
+++ Visual Studio Code Productivity Tips/Resource Files/CheckMySpelling.js
@@ -1,13 +1,11 @@
 // add a spell check extension to VS Code
 // most can handle errors in all the common text formats,
-// including JavaScript, Typescript, HTML, JSON, C# 
+// including JavaScript, Typescript, HTML, JSON, C#
 
 function LiveDemo() {
-  
-  var message;
-  message = "Welcome to Linkedn Academey"
-  
-  var errorMessage;
-  errorMessage = "Cannott complete the action!";
+	var message;
+	message = "Welcome to Linkedin Academy";
 
+	var errorMessage;
+	errorMessage = "Cannot complete the action!";
 }
