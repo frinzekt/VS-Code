c64358ef154c30cd77a6b509614ff2f444022f69
frinzekt <frinzekt@gmail.com>
2019-07-17 13:26:57 +0800

    VSCPT - Format Code

--- Visual Studio Code Productivity Tips/Resource Files/FormatCode.js
+++ Visual Studio Code Productivity Tips/Resource Files/FormatCode.js
@@ -1,22 +1,15 @@
 function useFormatDocment() {
-  // force formatting
-  var total = 0 ;
-  var attendeeCount = 4;
-            var results;
-  for (i=0;i<20;i++)
-  {
-
-
-    total+=1;
-  }
-  result        = attendeeCount *   total;
-
+	// force formatting
+	var total = 0;
+	var attendeeCount = 4;
+	var results;
+	for (i = 0; i < 20; i++) {
+		total += 1;
+	}
+	result = attendeeCount * total;
 }
 
-function defaultJavaScriptRules()
-{
-
-  var attendeeCount = 4   ; 
-        var x = 6;
-        
+function defaultJavaScriptRules() {
+	var attendeeCount = 4;
+	var x = 6;
 }
